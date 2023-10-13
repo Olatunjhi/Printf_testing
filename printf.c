99a1ae0c00f3faa7035dd5abcd4998cf4d81d377
olatunjhi <kinghorlatunjhi@gmail.com>
2023-10-13 18:50:11 +0100

    updated

---
+++ printf.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+#include <stdarg.h>
+
+int _printf(const char *format, ...)
+{
+	int count = 0;
+
+	va_list args;
+
+	va_start(args, format);
+
+	for (int i = 0; format[i] != '\0'; i++)
+	{
+		if (format[i] != '%')
+		{
+			printf("%c", format[i]);
+			count++;
+		}
+		else
+		{
+			switch (format[i + 1])
+			{
+				case 'c':
+					printf("%c", va_arg(args, int));
+					count++;
+					break;
+				case 's':
+					printf("%s", va_arg(args, char *));
+					count++;
+					break;
+				case '%':
+					printf("%%");
+					count++;
+					break;
+				default:
+
+				printf("%%%c", format[i]);
+				count += 2;
+			}
+		}
+	}
+
+	va_end(args);
+
+	return (count);
+}
